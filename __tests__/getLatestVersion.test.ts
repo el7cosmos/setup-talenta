00eadac955ad0eab3e614d6abed960540c996bdf
Abi أب <el@elabee.me>
2021-05-17 15:30:01 +0700

    @actions/github@5.0.0

--- __tests__/getLatestVersion.test.ts
+++ __tests__/getLatestVersion.test.ts
@@ -1,18 +1,18 @@
 import {getLatestVersion} from '../src/getLatestVersion'
 
-jest.mock('@actions/github', () => {
-  return {
-    getOctokit: jest.fn().mockReturnValue({
+jest.mock('@actions/github', () => ({
+  getOctokit: jest.fn().mockReturnValue({
+    rest: {
       repos: {
-        getLatestRelease: jest.fn().mockReturnValue({
+        getLatestRelease: jest.fn().mockResolvedValue({
           data: {
             tag_name: '0.0.1'
           }
         })
       }
-    })
-  }
-})
+    }
+  })
+}))
 
 test('getLatestVersion', async () => {
   await expect(getLatestVersion('token')).resolves.toBe('0.0.1')
--- src/getLatestVersion.ts
+++ src/getLatestVersion.ts
@@ -3,7 +3,7 @@ import * as github from '@actions/github'
 export async function getLatestVersion(token: string): Promise<string> {
   const octokit = github.getOctokit(token)
 
-  const {data} = await octokit.repos.getLatestRelease({
+  const {data} = await octokit.rest.repos.getLatestRelease({
     owner: 'el7cosmos',
     repo: 'talenta'
   })
